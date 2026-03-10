9b05c3a48f6d903b7d2b78581cc0918a7c8fa620
AbdulRahman AbdulJalil <117041593+eng-abdulr7man@users.noreply.github.com>
2026-03-10 20:34:23 +0200

    Update RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -54,7 +54,7 @@ const RegisterPage = () => {
                 <Sprout className="w-6 h-6 text-primary" />
               </div>
               <span className="font-tajawal font-bold text-xl">
-                MuAgri<span className="text-primary">Smart</span>
+                نـَـبْـتـَـة
               </span>
             </Link>
             <h1 className="text-2xl font-black text-foreground">إنشاء حساب جديد</h1>
