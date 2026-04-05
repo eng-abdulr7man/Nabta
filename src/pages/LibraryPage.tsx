62efd86a45f1e4923512cbda88fccd8e7d76458c
AbdulRahman AbdulJalil <117041593+eng-abdulr7man@users.noreply.github.com>
2026-04-06 01:50:13 +0200

    Update LibraryPage.tsx

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -72,7 +72,7 @@ const LibraryPage = () => {
             </motion.div>
             <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 }} className="text-4xl md:text-5xl font-black text-white mb-6 leading-tight">
               كل ماتحتاجه للتفوق في <span className="text-emerald-500">كليتك</span>
-            </motion.div>
+            </motion.h1>
             <motion.p initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.2 }} className="text-neutral-400 text-lg mb-10">
               محاضرات، سكاشن، وتسجيلات لمختلف المواد الزراعية مصنفة حسب جامعتك.
             </motion.p>
